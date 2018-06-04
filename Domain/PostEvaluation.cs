082667872b17ae52dca35b35f559a74fc7a84e5d
Mikael <mikaelkarlgren@hotmail.se>
2018-06-04 19:03:59 +0200

    Added Post and Evaluation objects

--- Domain/PostEvaluation.cs
+++ Domain/PostEvaluation.cs
@@ -10,5 +10,8 @@ namespace Domain
     {
         public int PostID { get; set; }
         public int EvaluationID { get; set; }
+
+        public Post Post { get; set; }
+        public Evaluation Evaluation { get; set; }
     }
 }
