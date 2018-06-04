6a4bd9d2f7f6bac7186ee55596934b505111dd7e
Mikael <mikaelkarlgren@hotmail.se>
2018-06-04 22:24:39 +0200

    Implemented logging

--- Data/BlogContext.cs
+++ Data/BlogContext.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
 
 namespace Data
 {
@@ -16,6 +18,13 @@ namespace Data
         public DbSet<Evaluation> Evaluations { get; set; }
         public DbSet<PostEvaluation> PostEvaluations { get; set; }
 
+        public static readonly LoggerFactory MyLoggerFactory
+            = new LoggerFactory(new[]
+            {
+                new ConsoleLoggerProvider((category, level) => 
+                category == DbLoggerCategory.Database.Command.Name && level == LogLevel.Information, true)
+            });
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<User>().ToTable("User");
@@ -23,6 +32,13 @@ namespace Data
             modelBuilder.Entity<Post>().ToTable("Post");
             modelBuilder.Entity<Evaluation>().ToTable("Evaluation");
             modelBuilder.Entity<PostEvaluation>().ToTable("PostEvaluation").HasKey(m => new { m.PostID, m.EvaluationID });
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .EnableSensitiveDataLogging()
+                .UseLoggerFactory(MyLoggerFactory);
         }
     }
 }
