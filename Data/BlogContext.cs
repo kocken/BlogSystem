714f6f05d2170b8a29105736e91cc01da4b77353
Mikael <mikaelkarlgren@hotmail.se>
2018-06-06 15:39:01 +0200

    Change to pass options to derived class

--- Data/BlogContext.cs
+++ Data/BlogContext.cs
@@ -5,6 +5,9 @@ namespace Data
 {
     public class BlogContext : DbContext
     {
+
+        public BlogContext(DbContextOptions<BlogContext> options) : base(options) {}
+
         public DbSet<User> Users { get; set; }
         public DbSet<Thread> Threads { get; set; }
         public DbSet<Post> Posts { get; set; }
