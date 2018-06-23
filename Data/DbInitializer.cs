27197cea0f4aa2f2752342e7246ac22a1c155a84
Mikael <mikaelkarlgren@hotmail.se>
2018-06-23 18:58:05 +0200

    Added  & changed some data in DbInitializer for better presenting

--- Data/DbInitializer.cs
+++ Data/DbInitializer.cs
@@ -49,13 +49,13 @@ namespace Data
             User[] users = new User[]
             {
                 new User{
-                    Username = "Mikael",
+                    Username = "Admin",
                     Password = "pass123",
                     Rank = Array.Find(ranks, r => r.Name.Equals(Ranks.Administrator.ToString())),
                     JoinTime = DateTime.Now
                 },
                 new User{
-                    Username = "Mikael2",
+                    Username = "Mikael",
                     Password = "pass123",
                     Rank = Array.Find(ranks, r => r.Name.Equals(Ranks.Moderator.ToString())),
                     JoinTime = DateTime.Now + TimeSpan.FromSeconds(5)
@@ -77,15 +77,21 @@ namespace Data
             Thread[] threads = new Thread[]
             {
                 new Thread{
-                    User = Array.Find(users, u => u.Username.Equals("Mikael")),
-                    Title = "First thread",
+                    User = Array.Find(users, u => u.Username.Equals("Admin")),
+                    Title = "#1",
                     Message = "This is the first thread made!",
                     CreationTime = DateTime.Now
                 },
                 new Thread{
                     User = Array.Find(users, u => u.Username.Equals("Mikael")),
                     Title = "Second thread test",
-                    Message = "Just a test",
+                    Message = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                    "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                    "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris " +
+                    "nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in " +
+                    "reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla" +
+                    " pariatur. Excepteur sint occaecat cupidatat non proident, sunt in " +
+                    "culpa qui officia deserunt mollit anim id est laborum.",
                     CreationTime = DateTime.Now + TimeSpan.FromSeconds(30)
                 }
             };
@@ -141,8 +147,40 @@ namespace Data
                 new Comment{
                     User = Array.Find(users, u => u.Username.Equals("Billy")),
                     Thread = threads[0],
-                    Message = "Can I get mod?",
+                    Message = "Disapprove this comment",
                     CreationTime = DateTime.Now + TimeSpan.FromSeconds(40)
+                },
+                new Comment{
+                    User = Array.Find(users, u => u.Username.Equals("Mikael")),
+                    Thread = threads[1],
+                    Message = "Sed ut perspiciatis unde omnis iste natus error " +
+                    "sit voluptatem accusantium doloremque laudantium, totam rem" +
+                    " aperiam, eaque ipsa quae ab illo inventore veritatis et quasi" +
+                    " architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam" +
+                    " voluptatem quia voluptas sit aspernatur aut odit aut fugit, " +
+                    "sed quia consequuntur magni dolores eos qui ratione voluptatem" +
+                    " sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum " +
+                    "quia dolor sit amet, consectetur, adipisci velit, sed quia non" +
+                    " numquam eius modi tempora incidunt ut labore et dolore magnam" +
+                    " aliquam quaerat voluptatem. Ut enim ad minima veniam, quis " +
+                    "nostrum exercitationem ullam corporis suscipit laboriosam, " +
+                    "nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum" +
+                    " iure reprehenderit qui in ea voluptate velit esse quam nihil " +
+                    "molestiae consequatur, vel illum qui dolorem eum fugiat quo " +
+                    "voluptas nulla pariatur?",
+                    CreationTime = DateTime.Now + TimeSpan.FromSeconds(60)
+                },
+                new Comment{
+                    User = Array.Find(users, u => u.Username.Equals("Billy")),
+                    Thread = threads[0],
+                    Message = "Can I get mod?",
+                    CreationTime = DateTime.Now + TimeSpan.FromSeconds(80)
+                },
+                new Comment{
+                    User = Array.Find(users, u => u.Username.Equals("Billy")),
+                    Thread = threads[0],
+                    Message = "Hello???",
+                    CreationTime = DateTime.Now + TimeSpan.FromSeconds(600)
                 }
             };
             foreach (Comment c in comments.Reverse())
@@ -169,14 +207,26 @@ namespace Data
                 new Evaluation{
                     Comment = comments[0],
                     EvaluationValue = Array.Find(evaluationValues, e => e.Name.Equals(EvaluationValues.Approved.ToString())),
-                    EvaluatedBy = Array.Find(users, u => u.Username.Equals("Mikael")),
+                    EvaluatedBy = Array.Find(users, u => u.Username.Equals("Admin")),
                     EvaluationTime = DateTime.Now + TimeSpan.FromSeconds(40)
                 },
                 new Evaluation{
                     Comment = comments[0],
                     EvaluationValue = Array.Find(evaluationValues, e => e.Name.Equals(EvaluationValues.Approved.ToString())),
-                    EvaluatedBy = Array.Find(users, u => u.Username.Equals("Mikael2")),
+                    EvaluatedBy = Array.Find(users, u => u.Username.Equals("Mikael")),
                     EvaluationTime = DateTime.Now + TimeSpan.FromSeconds(60)
+                },
+                new Evaluation{
+                    Comment = comments[1],
+                    EvaluationValue = Array.Find(evaluationValues, e => e.Name.Equals(EvaluationValues.Disapproved.ToString())),
+                    EvaluatedBy = Array.Find(users, u => u.Username.Equals("Mikael")),
+                    EvaluationTime = DateTime.Now + TimeSpan.FromSeconds(80)
+                },
+                new Evaluation{
+                    Comment = comments[2],
+                    EvaluationValue = Array.Find(evaluationValues, e => e.Name.Equals(EvaluationValues.Approved.ToString())),
+                    EvaluatedBy = Array.Find(users, u => u.Username.Equals("Mikael")),
+                    EvaluationTime = DateTime.Now + TimeSpan.FromSeconds(100)
                 }
             };
             foreach (Evaluation e in evaluations.Reverse())
